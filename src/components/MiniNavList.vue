98616b02e481996a500485a9e22d9722c3040fd5
Yow-Long Lee <yowlonglee@gmail.com>
2019-07-21 12:30:11 +0800

    Add mini nav component

---
+++ src/components/MiniNavList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mini-nav-list">
+    <router-link class="mini-nav-item" to="/to-do-list">
+      <i class="material-icons md-36">list</i>
+      <div>to-do list</div>
+    </router-link>
+    <router-link class="mini-nav-item" to="/analytics">
+      <i class="material-icons md-36">insert_chart</i>
+      <div>analytics</div>
+    </router-link>
+    <router-link class="mini-nav-item" to="/ringtones">
+      <i class="material-icons md-36">library_music</i>
+      <div>ringtones</div>
+    </router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: "MiniNavList"
+};
+</script>
+<style lang="scss" scoped></style>
